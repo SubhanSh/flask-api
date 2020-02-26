76eb38592d2a2908161924ecff66e15616816b25
Subhan <mrsubhan.sh@gmail.com>
2020-02-26 13:40:33 +0330

    get and post test

--- resources/Hello.py
+++ resources/Hello.py
@@ -1,6 +1,17 @@
 from flask_restful import Resource
+from markupsafe import escape
+from flask import request
 
 
 class Hello(Resource):
     def get(self):
-        return {"message": "Hello, World!"}
+        return {'message': 'Hello sir'}
+
+    def post(self):
+        # json_data = request.get_json(force=True)
+        # if not json_data:
+        #     return {'message': 'The data has send should be json'}, 400
+        result = {'key one': 'val one', 'key two': 'val two'}
+        error = None
+
+        return {'status': 'success', 'error': error, 'data': result}, 201
