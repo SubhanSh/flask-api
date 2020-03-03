f811f020d6dcde94188e65fdfda84824242102ef
Subhan <mrsubhan.sh@gmail.com>
2020-03-03 16:08:38 +0330

    uncomment

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
-#!/usr/bin/env python
 from flask import Flask
+# from flask_cors import CORS
 
-print("Content-type: text/html\n\n")
-print("<html><body><h1>salam</h1></body></html>")
+
+def create_app(config_filename):
+    app = Flask(__name__)
+    # CORS(app)
+    app.config.from_object(config_filename)
+
+    from app import api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app("config")
+    app.run(debug=True)
